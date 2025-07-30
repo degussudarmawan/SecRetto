620b01e2ee052711e5c36b47925c933947298e98
Gede Agus Sudarmawan <z5490930@ad.unsw.edu.au>
2025-07-30 21:51:57 +1000

    fix: fixed backend url

--- frontend/src/app/signup/page.tsx
+++ frontend/src/app/signup/page.tsx
@@ -13,7 +13,7 @@ export default function SignUpPage() {
     setIsLoading(true);
     try {
       const response = await fetch(
-        "${process.env.NEXT_PUBLIC_API_URL}/api/auth/google",
+        `${process.env.NEXT_PUBLIC_API_URL}/api/auth/google`,
         {
           method: "POST",
           headers: { "Content-Type": "application/json" },
